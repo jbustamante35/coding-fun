9f151ce8c30ee3c528217f911c020d2e9f86a0a3
Julian Bustamante <jbustamante35@gmail.com>
2018-10-27 03:20:16 -0500

    Updates from rin branch. Some python and kotlin additions.

--- python/apps/ExpenseTracker/Expense.py
+++ python/apps/ExpenseTracker/Expense.py
@@ -3,33 +3,33 @@ from getMonthFactor import getMonthFactor
 
 class Expense:
     def __init__(self, nm, desc, ctg, cp, fq):
-        self.name = nm
-        self.description = desc
-        self.category = ctg
-        self.cost_paid = cp
-        self.frequency = fq
+        self.name         = nm
+        self.description  = desc
+        self.category     = ctg
+        self.cost_paid    = cp
+        self.frequency    = fq
         self.monthly_cost = 0
 
     def getName(self):
         return self.name
-    
+
     def getDescription(self):
         return self.description
-    
+
     def getCategory(self):
         return self.category
-    
+
     def getCostPaid(self):
         return self.cost_paid
-    
+
     def getFrequency(self):
         return self.frequency
-    
+
     def getMonthlyCost(self):
         return self.monthly_cost
 
     def calculateMonthly(self):
-        fac = getMonthFactor(self.frequency)
+        fac               = getMonthFactor(self.frequency)
         self.monthly_cost = self.cost_paid / fac
         return self.monthly_cost
 
@@ -37,11 +37,12 @@ class Expense:
         print(
         "Expense: {:s}\n"
         "Description: {:s}\n"
+        "Category: {:s}\n"
         "Payment Interval: {:s}\n"
         "Total Paid: {:f}\n"
         "Calculated Montly: {:f}\n"
         .format(
-                self.name, self.description, self.frequency,
-                self.cost_paid, self.monthly_cost))
-        
-        
+                self.name, self.description, self.category,
+                self.frequency, self.cost_paid, self.monthly_cost))
+
+
--- python/apps/ExpenseTracker/FileHandler.py
+++ python/apps/ExpenseTracker/FileHandler.py
@@ -1,31 +1,39 @@
 # Module for reading and writing Expense object details
 from __future__ import print_function
 from Expense import Expense
+import csv
 
-def expensesFromFile(fin, delim):
-    flines = fin.readlines()
-    exps = []
-    n = 0
-    for fline in flines:
-        fline.rstrip()
-        print('in: {:s}'.format(fline), end='')
-        split_line = fline.split(delim)
-        exps.append(makeExpense(split_line))
-        n += 1
-    return exps
+class FileHandler:
+    def __init__(self, fin, fout, delim):
+        self.fin   = fin
+        self.fout  = fout
+        self.delim = delim
 
-def makeExpense(split_line):
-    print('split: {:s}'.format(split_line), end='')
-    name = split_line[0]
-    cost = float(split_line[1])
-    freq = split_line[2]
-    description = split_line[len(split_line)-1]
-    exp = Expense(name, description, 'noCategory', cost, freq)
-    exp.calculateMonthly()
-    return exp
+    def expensesFromFile(self, fin, delim):
+        flines = fin.readlines()
+        exps   = []
+        n      = 0
+        for fline in flines:
+            fline.rstrip()
+            #print('in: {:s}'.format(fline), end='')
+            split_line = fline.split(delim)
+            exps.append(self.makeExpense(split_line))
+            n += 1
+        return exps
 
-def write2file(exp, fout):
-    out = exp.printExpense()
-    fout.write(out)
-    return 1
+    def makeExpense(self, split_line):
+        #print('split: {:s}'.format(split_line), end='')
+        name        = split_line[0]
+        cost        = float(split_line[1])
+        freq        = split_line[2]
+        description = split_line[len(split_line)-1]
+        exp         = Expense(name, description, 'noCategory', cost, freq)
+        exp.calculateMonthly()
+        return exp
+
+    def write2file(self, exp, fout):
+        fout.open(fout.name, 'a+')
+        out = exp.printExpense()
+        fout.write(out)
+        return 1
 
