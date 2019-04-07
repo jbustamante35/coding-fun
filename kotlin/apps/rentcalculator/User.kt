5ccd5cc45cb13cb93dd62a57f9babf52d1fe3436
Julian Bustamante <jbustamante35@gmail.com>
2019-04-06 22:17:20 -0500

    winry update after a while, unsure what exactly I changed

--- kotlin/apps/rentcalculator/User.kt
+++ kotlin/apps/rentcalculator/User.kt
@@ -22,35 +22,35 @@ Methods should calculate:
  */
 
 fun main (args: Array<String>) {
-		var user1 = User("akire")
-		var user2 = User("julian")
-		var user3 = User("samara")
+    var user1 = User("akire")
+    var user2 = User("julian")
+    var user3 = User("samara")
 
-		println("Current Users: ${user1.name} | ${user2.name} | ${user3.name}")
-		var rat = user1.calculateRatio(9.0, 31.0, "MGE")
-		println("MGERatio = $rat")
+    println("Current Users: ${user1.name} | ${user2.name} | ${user3.name}")
+    var rat = user1.calculateRatio(9.0, 31.0, "MGE")
+    println("MGERatio = $rat")
 
-		var tot = user2.calculateTotal(rat, 835.00, 2, "Rent")
-		println("RentTotal = $tot")
+    var tot = user2.calculateTotal(rat, 835.00, 2, "Rent")
+    println("RentTotal = $tot")
 }
 
 class User (_name: String) {
-		val name = _name.capitalize()
-		init {
-				println("Created user...$name")
-		}
+    val name = _name.capitalize()
+    init {
+    	println("Created user...$name")
+    }
 
-		fun calculateRatio(days: Double, total: Double, param: String): Double {
-				// Calculate the percentage of total days user will pay for param
-				println("Percentage $param | $days / $total")
-				return days / total
-		}
+    fun calculateRatio(days: Double, total: Double, param: String): Double {
+        // Calculate the percentage of total days user will pay for param
+        println("Percentage $param | $days / $total")
+        return days / total
+    }
 
-		fun calculateTotal(ratio: Double, cost: Double, users: Int, param: String): Double {
-				// Calculate total user cost for param
-				println("User Share $param | ($ratio * $cost) / $users")
-				return (ratio * cost) / users
-		}
+    fun calculateTotal(ratio: Double, cost: Double, users: Int, param: String): Double {
+        // Calculate total user cost for param
+        println("User Share $param | ($ratio * $cost) / $users")
+        return (ratio * cost) / users
+    }
 
 }
 
--- kotlin/apps/rentcalculator/UserInfo.kt
+++ kotlin/apps/rentcalculator/UserInfo.kt
@@ -1,8 +1,9 @@
 fun main (args: Array<String>) {
     if (args.size == 0) {
-			println("enter a command-line argument")
-			return
-	}
-	println("Hwllow whorled. You must be ${args[0]}")
+        println("enter a command-line argument")
+        return
+    }
+
+    println("Hwllow whorled. You must be ${args[0]}")
 }
 
